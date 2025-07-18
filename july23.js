8936409d245fdc80191070138c9e34a7e1e6f4f9
lokendra0202 <loksingh468@gmail.com>
2025-07-18 14:45:43 +0530

    All Assignment

--- july23.js
+++ july23.js
@@ -2,8 +2,10 @@ const EventEmitter = require('events');
 const myEmitter = new EventEmitter();
 function eventlist (eventName) {
     console.log(`Callback triggerd by event: ${eventName}`);
+    myEmitter
 }
 ['eventA', 'eventB', 'eventc'].forEach(event => {
     myEmitter.on(event, () => eventlist(event));
 });
 myEmitter.emit('eventB');
+myEmitter.emit('eventA');
